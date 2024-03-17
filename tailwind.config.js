be295a8ead8d8ccf34ebdf971f9ab79d3e93d942
Flávia Reis <flaviarster@gmail.com>
2024-03-17 10:34:57 -0300

    I added the font family, the custom colors and started to make the styles and the HTML structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Poppins': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        purple: '#854DFF',
+        red: '#FF5959',
+        black: '#151515',
+        grey: '#716F6F',
+        light_grey: '#DCDCDC',
+        off_white: 'hsl(0, 0%, 94%)',
+      },
+    },
   },
   plugins: [],
 }
